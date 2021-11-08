8c95f019d64f25101ad71aea8db2dce154e37ac6
david <davidlimjh77@gmail.com>
2021-11-08 23:01:07 +0800

    base

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import methodOverride from 'method-override';
 
 import {
-  getAllSightings, getSightingForm, postSightingForm, getSightingByIndex, getSightingsEditForm,
+  getAllSightings, getSightingForm, postSightingForm, getSightingByIndex, getSightingsEditForm, editSightings, getSightingsShapeList, getSightingsByShape, deleteSighting,
 } from './routes.js';
 
 const PORT = 3001;
@@ -9,13 +10,18 @@ const PORT = 3001;
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', getAllSightings);
 app.get('/sighting', getSightingForm);
 app.post('/sighting', postSightingForm);
+app.put('/sighting/:index/edit', editSightings);
 app.get('/sighting/:index', getSightingByIndex);
 app.get('/sighting/:index/edit', getSightingsEditForm);
+app.get('/shapes', getSightingsShapeList);
+app.get('/shapes/:shape', getSightingsByShape);
+app.delete('/sighting/:index/delete', deleteSighting);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
--- routes.js
+++ routes.js
@@ -22,15 +22,14 @@ export const getSightingByIndex = (req, res) => {
       res.status(404).send({ error: err });
     }
     const sighting = jsonObj.sightings[index];
-    res.status(200).send({ message: 'successful' });
-    res.render('sighting', sighting);
+    res.render('sighting', { ...sighting, index });
   });
 };
 
 export const postSightingForm = (req, res) => {
   add('sightings', req.body, (err, data) => {
-    console.log('added');
-    console.log(data);
+    const parsedData = JSON.parse(data);
+    res.redirect(`/sighting/${parsedData.sightings.length - 1}`);
   });
 };
 
@@ -45,6 +44,59 @@ export const getSightingsEditForm = (req, res) => {
       res.status(404).send({ err });
     }
     const sighting = jsonObj.sightings[index];
-    res.render('sightings-edit-form', sighting);
+    res.render('sightings-edit-form', { ...sighting, index });
   });
+};
+
+export const editSightings = (req, res) => {
+  const { index } = req.params;
+  console.log(index, 'editing this index');
+  const readCallback = (err, jsonObj) => {
+    console.log(jsonObj);
+    jsonObj.sightings[index] = req.body;
+  };
+
+  edit(readCallback, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.redirect(`/sighting/${index}`);
+  });
+};
+
+export const getSightingsShapeList = (req, res) => {
+  read((err, jsonObj) => {
+    if (err) {
+      console.log(err);
+    }
+
+    const shapesArray = [
+      ...new Set(
+        jsonObj.sightings.map((sighting) => sighting.shape.toLowerCase()),
+      ),
+    ];
+
+    res.render('shapes-list', { shapes: shapesArray });
+  });
+};
+
+export const getSightingsByShape = (req, res) => {
+  const { shape } = req.params;
+  read((err, jsonObj) => {
+    const sightings = jsonObj.sightings.filter((sighting) => sighting.shape.toLowerCase().includes(shape));
+    res.render('shapes', { sightings, shape });
+  });
+};
+
+export const deleteSighting = (req, res) => {
+  const { index } = req.params;
+
+  const handleReadCallback = (err, jsonObj) => {
+    jsonObj.sightings.splice(index);
+  };
+
+  edit(handleReadCallback, (err) => console.log(err));
+
+  res.redirect('/');
 };
